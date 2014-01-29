b2f5cf7ae6f5848db24c9f7561639e829702496e
Ed Lee <eddieberklee@gmail.com>
2014-01-29 04:23:12 -0800

    automated pushing

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
+git add .
+git ci -am "automated pushing"
 git remote add website ssh://ehdeelee_compscieddy@ssh.phx.nearlyfreespeech.net/home/public/.git
 git push origin master
 git push website master
